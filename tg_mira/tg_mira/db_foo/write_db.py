7ae75b7130fe891efc63b0a3b9ea14db72cf6554
aleksey yakovlev <aleksey.yakovlev.i@zyfra.com>
2022-11-16 15:25:57 +0300

    Add in ".env" number id for database

--- tg_mira/tg_mira/db_foo/write_db.py
+++ tg_mira/tg_mira/db_foo/write_db.py
@@ -1,12 +1,13 @@
 from . import db_connect
 from sqlalchemy import exc
+from dotenv import dotenv_values
 
 
 def update_users(json_users, connection=db_connect.connect()):
     try:
         connection.execute("UPDATE bot_mira "
                            "SET safe=%s"
-                           "WHERE id=1;", (json_users,))
+                           f"WHERE id={dotenv_values('.env').get('ID_IN_DB')};", (json_users,))
     except exc.SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         print(error)
